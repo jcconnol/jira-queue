eced4626b157fe096310c00f239902b003db5e8c
John Connolly <jcconnol4@gmail.com>
2022-05-01 20:40:20 -0500

    changing api output

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "jiraqueue4d7b6aac": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "JiraQueue": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
--- pages/api/ticket-info.js
+++ pages/api/ticket-info.js
@@ -3,6 +3,7 @@
 //TODO save ticket info to file
 //TODO delete ticket from file
 const fs = require('fs')
+const ticketData = require('./getTicketInfo')
 
 export default async function userHandler(req, res) {
   const {
@@ -12,48 +13,18 @@ export default async function userHandler(req, res) {
 
   switch (method) {
     case 'GET':
+      var ticketDataList;
       await fs.readFile('./pages/api/ticketList.json', 'utf8', async function (err,data) {
         if (err) {
           console.log(err)
           return ['Error reading tickets'];
         }
 
-        var ticketList = JSON.parse(data).tickets;
-        var ticketDataList = [];
-        
-        for(var i=0; i < ticketList.length; i++){
-          var ticket = ticketList[i]
-
-          var headers = {
-            headers: {
-              "Authorization": "Basic amNjb25ub2w0QGdtYWlsLmNvbTpmTGREUXVqem9IeGJxd0xUeENEejkwRDA="
-            }, 
-            method: 'GET'
-          }
-
-          var endpoint = "https://jcconnol4.atlassian.net/rest/api/2/issue/" + ticket
-
-          var requestData = await fetch(endpoint, headers);
-
-          var jsonResp = await requestData.json();
-          var avatarImage = jsonResp.fields.project.avatarUrls["32x32"];
-          var summary = jsonResp.fields.summary;
-          var name = jsonResp.fields.name;
-          var key = jsonResp.key;
-
-          var ticketData = {
-            "avatarImage":avatarImage,
-            "summary":summary,
-            "name": name,
-            "key": key
-          }
-
-          ticketDataList.push(ticketData)            
-        }
-
-        res.status(200).json(ticketDataList);
+        ticketDataList = await ticketData.formatTicketData(data);
+        //res.status(200).json(ticketDataList);
       });
 
+      return ticketDataList
       break
     case 'POST':
       // Update, add or delete ticket
