0eeb2811d4fab4025405793ebfd3a3d8159b3788
John Connolly <jcconnol4@gmail.com>
2022-05-27 20:03:52 -0500

    adding editing icon options

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-          <Route path='/' element={<ShowQueue />} />
+          <Route path='/' element={<EditQueue />} />
           <Route exact path='/edit' element={<EditQueue/>} />
           <Route component={Page404} />
       </Routes>
